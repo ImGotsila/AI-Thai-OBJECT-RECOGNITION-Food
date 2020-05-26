00f90dff1fe4063bb4019372e2751a139888a7ef
ImGosila <47608559+GotApiwat@users.noreply.github.com>
2020-05-26 18:16:53 +0700

    first commit

---
+++ api.php
@@ -0,0 +1,41 @@
+﻿<?php
+if (isset($_GET['pathImg'])) {
+  // collect value of input field
+  $my_file = $_GET['pathImg'];
+  if (empty($my_file)) {
+    echo "Name is empty";
+  } else {
+    $curl = curl_init();
+    $img_file = $my_file;
+    $img_data = file_get_contents($img_file);
+    $data = json_encode(array("file" => base64_encode($img_data)));
+      
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://api.aiforthai.in.th/thaifood",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_POSTFIELDS => $data,
+      CURLOPT_HTTPHEADER => array(
+        "Content-Type: application/json",
+        "apikey: 6M75xhanxA84GoBEWUQ8qDmIrUxmqMpe"
+      )
+    ));
+    
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    
+    curl_close($curl);
+    
+    if ($err) {
+      echo "cURL Error #:" . $err;
+    } else {
+      echo ($response);
+    }
+  }
+}
+
+?>
---
+++ app.js
@@ -0,0 +1,36 @@
+﻿new Vue({
+    el:"#app",
+    mounted() {
+        console.log('Component mounted.')
+    },
+    data() {
+        return {
+          pathImg: 'images/img.jpg',
+          nameFood:'Select an New image',
+          description: null,
+          fileName:"",
+          objects:"",
+          state: true,
+        };
+    },
+    methods: {
+        formSubmit(e) {
+            e.preventDefault();
+            var self = this;
+            axios.get('api.php', {
+                params: {
+                    pathImg: this.pathImg
+                }
+            })
+            .then(function (response) {
+                self.description = response.data;   
+                self.fileName = self.description.filename;
+                self.objects = self.description.objects;                
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        }
+
+    }
+})
